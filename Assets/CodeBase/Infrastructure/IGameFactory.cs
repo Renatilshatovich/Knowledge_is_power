cce3b1d85ed0bc0b95a83209af3b7c14a6640b4d
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-08-18 14:21:12 +0500

    Factory

---
+++ Assets/CodeBase/Infrastructure/IGameFactory.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public interface IGameFactory
+    {
+        GameObject CreateHero(GameObject at);
+        void CreateHud();
+    }
+}
