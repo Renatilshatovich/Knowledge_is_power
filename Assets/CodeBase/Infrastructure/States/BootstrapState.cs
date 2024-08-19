bea2a5f9cb019e02e2c3f56f534426cd39e632d1
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-08-19 23:46:48 +0500

    Sevice Locator

---
+++ Assets/CodeBase/Infrastructure/States/BootstrapState.cs
@@ -0,0 +1,49 @@
+using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.States
+{
+    public class BootstrapState : IState
+    {
+        private const string Initial = "Initial";
+        private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
+
+        public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader)
+        {
+            _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
+        }
+        
+        public void Enter()
+        {
+            RegisterServices();
+            _sceneLoader.Load(Initial, EnterLoadLevel);
+        }
+
+        private void EnterLoadLevel() => _stateMachine.Enter<LoadLevelState, string>("Main");
+
+        private void RegisterServices()
+        {
+            Game.InputService = RegisterInputService();
+
+            AllServices.Container.RegisterSingle<IGameFactory>(new GameFactory(AllServices.Container.Single<IAssets>()));
+        }
+
+        public void Exit()
+        {
+            
+        }
+        
+        private static IInputService RegisterInputService()
+        {
+            if (Application.isEditor)
+                return new StandaloneInputService();
+            else
+                return new MobileInputService();
+        }
+    }
+}
