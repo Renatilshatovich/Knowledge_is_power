02be73c77839c5f22aa6d83c551d338e23153c5a
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-08-31 15:11:31 +0500

    Aggro

---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+        
+    [RequireComponent(typeof(Collider))]
+    public class Aggro: MonoBehaviour
+    {
+        public TriggerObserver TriggerObserver;
+        public AgentMoveToHero Follow;
+        
+        private void Start()
+        {
+            TriggerObserver.TriggerEnter += TriggerEnter;
+            TriggerObserver.TriggerExit += TriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void TriggerEnter(Collider obj) => 
+            SwitchFollowOn();
+
+        private void TriggerExit(Collider obj) => 
+            SwitchFollowOff();
+
+        private void SwitchFollowOn() => 
+            Follow.enabled = true;
+
+        private void SwitchFollowOff() => 
+            Follow.enabled = false;
+    }
+}
