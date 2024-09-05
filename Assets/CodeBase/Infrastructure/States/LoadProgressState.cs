d1db68a1b694d149252df4385ed5082b9be33e10
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-09-05 14:12:15 +0500

    Hero Attack

--- Assets/CodeBase/Infrastructure/States/LoadProgressState.cs
+++ Assets/CodeBase/Infrastructure/States/LoadProgressState.cs
@@ -42,6 +42,8 @@ namespace CodeBase.Infrastructure.States
             var progress = new PlayerProgress(initialLevel: "Main");
 
             progress.HeroState.MaxHP = 50;
+            progress.HeroStats.Damage = 1f;
+            progress.HeroStats.DamageRadius = .5f;
             progress.HeroState.ResetHP();
             
             return progress;
