f6ae7dc044ca7c1c9334665639d9501ac8e7c020
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-09-10 17:00:18 +0500

    True Unique Id

--- Assets/CodeBase/Editor/UniqueIdEditor.cs
+++ Assets/CodeBase/Editor/UniqueIdEditor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.CodeDom.Compiler;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.Device;
@@ -14,13 +15,25 @@ namespace Editor
         {
             var uniqueId = (UniqueId)target;
 
+            if (IsPrefab(uniqueId))
+                return;
+                
             if (string.IsNullOrEmpty(uniqueId.Id))
                 Generate(uniqueId);
+            else
+            {
+                UniqueId[] uniqueIds = FindObjectsOfType<UniqueId>();
+                
+                if (uniqueIds.Any(other => other != uniqueId && other.Id == uniqueId.Id))
+                    Generate(uniqueId);
+            }
         }
+
+        private bool IsPrefab(UniqueId uniqueId) => uniqueId.gameObject.scene.rootCount == 0;
 
         private void Generate(UniqueId uniqueId)
         {
-            uniqueId.Id = Guid.NewGuid().ToString();
+            uniqueId.Id = $"{uniqueId.gameObject.scene.name}_{Guid.NewGuid().ToString()}";
 
             if (!Application.isPlaying)
             {
