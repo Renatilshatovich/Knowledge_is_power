ef10199f16f46526753933b308336032dedf386d
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-09-17 21:59:33 +0500

    Loot Piece

---
+++ Assets/CodeBase/Data/LootData.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CodeBase.Data
+{
+    [Serializable]
+    public class LootData
+    {
+        public int Collected;
+        public void Collect(Loot loot)
+        {
+            Collected += loot.Value;
+        }
+    }
+}
